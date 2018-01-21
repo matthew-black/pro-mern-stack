a7893d590acbbfdc49801a65faddfc356f10ee87
Matthew Black <matthewallenblack@gmail.com>
2018-01-21 15:03:06 -0600

    Set up npm run compile & npm run watch scripts in package.json.

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
     var contentNode = document.getElementById('contents');
-    var component = <h1>Hello World</h1>; //  Simple JSX component
-    ReactDOM.render(component, contentNode); //  Renders component inside content Node
+    var component = <h1>Hello World!</h1>;
+    ReactDOM.render(component, contentNode);
