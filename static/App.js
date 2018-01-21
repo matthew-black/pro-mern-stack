461a6b6669f8d3176a89dd5ccbae908b19659d08
Matthew Black <matthewallenblack@gmail.com>
2018-01-21 15:11:49 -0600

    Add ES2015 transpiling, use fancy new JS in App.jsx file.

--- static/App.js
+++ static/App.js
@@ -1,7 +1,16 @@
+'use strict';
+
 var contentNode = document.getElementById('contents');
+
+var continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
+var message = continents.map(function (c) {
+  return 'Hello ' + c + '!';
+}).join(' ');
+
 var component = React.createElement(
-    'h1',
-    null,
-    'Hello World!'
+  'p',
+  null,
+  message
 );
+
 ReactDOM.render(component, contentNode);
