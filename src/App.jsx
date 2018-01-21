7a82d6cb251264cc3125ca4894a0980b4759d209
Matthew Black <matthewallenblack@gmail.com>
2018-01-21 15:53:51 -0600

    Mock in components that I shall be making.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,41 @@
 const contentNode = document.getElementById('contents');
 
-const continents = ['Africa', 'America', 'Asia', 'Australia', 'Europe'];
-const message = continents.map(c => `Hello ${c}!`).join(' ');
+class IssueFilter extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for the Issue Filter.</div>
+    )
+  }
+}
 
-const component = <p>{message}</p>;
+class IssueTable extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for a table of Issues.</div>
+    )
+  }
+}
 
-ReactDOM.render(component, contentNode);
+class IssueAdd extends React.Component {
+  render() {
+    return (
+      <div>This is a placeholder for an Issue Add entry form.</div>
+    )
+  }
+}
+
+class IssueList extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Issue Tracker</h1>
+        <IssueFilter />
+        <hr />
+        <IssueTable />
+        <hr />
+        <IssueAdd />
+      </div>
+    );
+  }
+}
+ReactDOM.render(<IssueList />, contentNode);
