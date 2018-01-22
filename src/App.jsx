b8b644a233cdd506acf66e7475e55cfe1c96009d
Matthew Black <matthewallenblack@gmail.com>
2018-01-22 12:18:33 -0600

    Add error handling to loadData().

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ class IssueFilter extends React.Component {
 
 const IssueRow = (props) => (
   <tr>
-    <td>{props.issue.id}</td>
+    <td>{props.issue._id}</td>
     <td>{props.issue.status}</td>
     <td>{props.issue.owner}</td>
     <td>{props.issue.created.toDateString()}</td>
@@ -27,7 +27,7 @@ function IssueTable(props) {
   // readable when left like this. (This could look like the above 'const IssueRow'
   // that uses the arrow function, fwiw.)
   const issueRows = props.issues.map(issue => <IssueRow
-    key={issue.id} issue={issue} />);
+    key={issue._id} issue={issue} />);
   return (
     <table className="bordered-table">
       <thead>
@@ -89,18 +89,24 @@ class IssueList extends React.Component {
   }
 
   loadData() {
-    fetch('/api/issues').then(response =>
-      response.json()
-    ).then(data => {
-      console.log("Total count of records:", data._metadata.total_count);
-      data.records.forEach(issue => {
-        issue.created = new Date(issue.created);
-        if (issue.completionDate)
-          issue.completionDate = new Date(issue.completionDate);
-      });
-      this.setState({ issues: data.records });
+    fetch('/api/issues').then(response => {
+      if (response.ok) {
+        response.json().then(data => {
+          console.log("Total count of records:", data._metadata.total_count);
+          data.records.forEach(issue => {
+            issue.created = new Date(issue.created);
+            if (issue.completionDate)
+              issue.completionDate = new Date(issue.completionDate);
+          });
+            this.setState({ issues: data.records });
+        });
+      } else {
+        resposne.json().then(error => {
+          alert("Failed to fetch issues:" + error.message)
+        });
+      }
     }).catch(err => {
-      console.log(err);
+      alert("Error in fetching data from server:", err);
     });
   }
 
